ec46d39c4fb61251eb2ca97d62d529ff02bebdad
Ahmet Erbay <ahmeterbay020@gmail.com>
2022-09-17 23:40:02 +0300

    GreatNumber class updated

--- src/GreatNumber.java
+++ src/GreatNumber.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class GreatNumber {
     public static void main(String[] args) {
+       checkGreatnumber();
+    }
+    public static void  checkGreatnumber(){
+        Scanner sc = new Scanner(System.in);
         int num;
         int total=0;
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your number");
         num = sc.nextInt();
         for (int i=num-1;i>=1;i--){
-        if(num%i==0){
-           total+=i;
+            if(num%i==0){
+                total+=i;
+            }
         }
-    }
         if (total==num){
             System.out.println("Your number is great number\n" +
                     "Great job");
