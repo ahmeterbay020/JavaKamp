25f830a684ee8d95440850a71b639665e1fea3bb
Ahmet Erbay <ahmeterbay020@gmail.com>
2022-09-25 23:10:02 +0300

    reCap_Demo_classes class updated

---
+++ src/ReCapDemo/reCap_Demo_classes.java
@@ -0,0 +1,10 @@
+package ReCapDemo;
+
+public class reCap_Demo_classes {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(2,3);
+
+
+    }
+}
